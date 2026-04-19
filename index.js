cca4add0685d8a327679080e72b85949bf9ba94b
Bashir <bashir6412@gmail.com>
2026-04-19 21:01:12 +0400

    v1

---
+++ index.js
@@ -0,0 +1,32 @@
+let UserInput = document.getElementById("user_input");
+let GuessBtn = document.getElementById("guess_btn");
+let Result = document.getElementById("display_result");
+let CPUNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+function PickingNum() {
+    let RandomNum = CPUNum[Math.floor(Math.random() * CPUNum.length)]
+    return RandomNum;
+
+}
+
+let RandomNum = PickingNum();
+
+
+GuessBtn.addEventListener("click", function() {
+    let UserNum = parseFloat(UserInput.value);
+
+    if (UserNum === RandomNum) {
+        Result.textContent="You guessed it correctly"
+        console.log("Correct")        
+        let RandomNum = PickingNum();
+
+    } else if (UserNum > RandomNum) {
+        Result.textContent="Too High!"
+        console.log("Too High")
+
+    } else if (RandomNum > UserNum) {
+        Result.textContent="Too Low!"
+        console.log("Too Low")
+    }
+    
+});
