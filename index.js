abb6858e990acb6789774240f1d0a2927a346a26
Bashir <bashir6412@gmail.com>
2026-04-21 17:29:55 +0400

    ts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let UserInput = document.getElementById("user_input");
 let GuessBtn = document.getElementById("guess_btn");
 let Result = document.getElementById("display_result");
 let CPUNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let ResetBtn = document.getElementById("reset_btn")
 
 function PickingNum() {
     let RandomNum = CPUNum[Math.floor(Math.random() * CPUNum.length)]
@@ -10,7 +11,6 @@ function PickingNum() {
 }
 
 let RandomNum = PickingNum();
-
 
 GuessBtn.addEventListener("click", function() {
     let UserNum = parseFloat(UserInput.value);
@@ -29,4 +29,9 @@ GuessBtn.addEventListener("click", function() {
         console.log("Too Low")
     }
     
+});
+
+ResetBtn.addEventListener("onclick", function() {
+    RandomNum = PickingNum();
+    Reset.textContent=""
 });
