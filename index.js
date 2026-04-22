de06782ff770ed225e50055ac8df03a6d7bd7850
Bashir <bashir6412@gmail.com>
2026-04-22 19:39:03 +0400

    Issue : Updates Functionality and other realistcs

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+let TrackingIndex = [Clicks, Wins, Losses] = [0, 0, 0];
+let CPUNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 let UserInput = document.getElementById("user_input");
 let GuessBtn = document.getElementById("guess_btn");
 let Result = document.getElementById("display_result");
-let CPUNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let ResetBtn = document.getElementById("reset_btn")
+let ResetBTN = document.getElementById("reset_btn");
+let TrackingFeild = document.getElementById("tracking_feild");
+let LimitReachedFeild = document.getElementById("Limit_Reached");
+let ErrorFeild = document.getElementById("Error_Result");
 
 function PickingNum() {
     let RandomNum = CPUNum[Math.floor(Math.random() * CPUNum.length)]
     return RandomNum;
-
 }
+TrackingFeild.textContent = `Clicks: ${Clicks} Wins: ${Wins} Losses: ${Losses}`;
 
-let RandomNum = PickingNum();
 
+let RandomNum = PickingNum();    
+    
 GuessBtn.addEventListener("click", function() {
-    let UserNum = parseFloat(UserInput.value);
+        let UserNum = parseFloat(UserInput.value);
+        TrackingFeild.textContent = `Clicks: ${Clicks} Wins: ${Wins} Losses: ${Losses}`;
+        TrackingIndex[0] = [Clicks ++];
 
-    if (UserNum === RandomNum) {
-        Result.textContent="You guessed it correctly"
+   if (UserNum === RandomNum) {
+        TrackingIndex[1] = [Wins ++];
+        TrackingFeild.textContent = `Clicks: ${Clicks} Wins: ${Wins} Losses: ${Losses}`;
+        Result.textContent="You guessed it correctly";
         console.log("Correct")        
         let RandomNum = PickingNum();
-
+        
     } else if (UserNum > RandomNum) {
         Result.textContent="Too High!"
         console.log("Too High")
@@ -27,11 +36,59 @@ GuessBtn.addEventListener("click", function() {
     } else if (RandomNum > UserNum) {
         Result.textContent="Too Low!"
         console.log("Too Low")
+    } else if (UserNum < 0) {
+        ErrorFeild.textContent="Error! Please fill a valid Number"
+        console.log("ni")
+    };
+
+    if (TrackingIndex[0] == 5) {
+        TrackingIndex[2] = [Losses ++];
+        console.log("5 Limit Reached");
+        LimitReached();
+    } else if (TrackingIndex[0] == 10) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
+    } else if (TrackingIndex[0] == 10) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
+    } else if (TrackingIndex[0] == 15) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
+    } else if (TrackingIndex[0] == 20) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
+    } else if (TrackingIndex[0] == 25) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
+    } else if (TrackingIndex[0] == 30) {
+        TrackingIndex[2] = [Losses ++];
+        LimitReached();
     }
     
+    if (UserNum < 0)  {
+        ErrorFeild.textContent="Error! Please input a valid Num"
+    } else {
+        ErrorFeild.textContent=""
+    }
 });
 
-ResetBtn.addEventListener("onclick", function() {
+
+function Reset() { 
+    document.getElementById('user_input').value='';
     RandomNum = PickingNum();
-    Reset.textContent=""
+    Result.textContent="";
+    LimitReachedFeild.textContent=""
+}
+
+ResetBTN.addEventListener("click", function() {
+    Reset();
 });
+
+function LimitReached() {
+    LimitReachedFeild.textContent="Your 5 Click Limit has been reached"
+    setTimeout(Reset, 2100)
+
+}
+
+
+
